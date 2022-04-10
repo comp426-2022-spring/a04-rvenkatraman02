2f5238d10907b8a90fcb69f48bd35dab29d06fcd
rvenkatraman02 <rvenka@ad.unc.edu>
2022-04-10 01:24:15 -0400

    adding accesslog to where statement in database.js

--- database.js
+++ database.js
@@ -8,7 +8,7 @@ const db = new database('log.db');
 
 // Is the database initialized or do we need to initialize it?
 const stmt = db.prepare(`
-    SELECT name FROM sqlite_master WHERE type='table' and name='userinfo';`
+    SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`
     );
 // Define row using `get()` from better-sqlite3
 let row = stmt.get();
@@ -18,11 +18,11 @@ if (row === undefined) {
     console.log('Your database appears to be empty. Creating log database...');
 // Set a const that will contain your SQL commands to initialize the database.
     const sqlInit = `
-        CREATE TABLE IF NOT EXISTS accesslog ( 
+        CREATE TABLE accesslog ( 
             id INTEGER PRIMARY KEY, 
             remoteaddr VARCHAR, 
             remoteuser VARCHAR, 
-            datetime VARCHAR, 
+            time VARCHAR, 
             method VARCHAR, 
             url VARCHAR, 
             protocol VARCHAR,
@@ -36,7 +36,7 @@ if (row === undefined) {
     db.exec(sqlInit);
 } else {
 // Since the database already exists, echo that to the console.
-    console.log('Database exists.')
+    console.log('Log database exists.')
 }
 // Export all of the above as a module so that we can use it elsewhere.
 module.exports = db
